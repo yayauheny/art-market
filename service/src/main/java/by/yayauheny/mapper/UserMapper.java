77a263fc04e23810e6e97bc131aa77c59b10660d
Evgeny Leshok <work.evles@gmail.com>
2025-04-14 19:48:20 +0300

    remove comments from code

--- service/src/main/java/by/yayauheny/mapper/UserMapper.java
+++ service/src/main/java/by/yayauheny/mapper/UserMapper.java
@@ -15,14 +15,6 @@ import org.mapstruct.factory.Mappers;
 )
 public interface UserMapper {
 
-//  @Mapping(target = "username", qualifiedByName = {"UserMapperUtil", "getUsername"}, source = "email")
-//  @Mapping(target = "roles", qualifiedByName = {"UserMapperUtil", "setRoles"}, source = "role")
-//  @Mapping(target = "lastLogin", expression = "java(LocalDateTime.now())")
-//  @Mapping(target = "birthDate", dateFormat = "yyyy-MM-dd")
-//  @Mapping(target = "email", expression = "java(dto.getEmail().trim().toLowerCase())")
-//  UserEntity fromCreateRequest(UserCreateRequest dto);
-
-//  @Mapping(target = "roles", qualifiedByName = {"UserMapperUtil", "convertRolesToRoleNames"}, source = "roles")
   UserResponse toResponse(UserEntity user);
 
   List<UserResponse> toResponseList(List<UserEntity> users);
